cef268b6466cb0038aae56fc3c5fb78bbe18de44
Ivan Herrera <herre_ivan@hotmail.com>
2024-07-04 15:05:57 -0300

    se actualizo el css para darle estilos mas apropiados

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import tituloFooter from '../../../public/images/logo.png'
 
 export const Footer = () => {
   return (
     <footer className='footer'>
       <div className='footer-contenedor'>
-      <Link to='/AppleShipping/' className='header-contenedor-footer'><img src={tituloFooter} className='titulo-footer' /></Link>
+      <Link to='/AppleShipping/' className='header-contenedor-footer'></Link>
     <a href="https://github.com/Ivannherrera/ProyectoFinaReact" target="_blank">
               Todos los derechos reservados. <i className="fas fa-heart"></i>
             </a>
